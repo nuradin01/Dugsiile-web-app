b02e26687ea4fceb040d20ba5cd0efae36d0dfac
Nuradin Ismail <nuradin170@gmail.com>
2020-12-01 22:26:00 +0300

    deploying to heroku

--- server.js
+++ server.js
@@ -63,6 +63,21 @@ app.use('/api/v1/auth', auth);
 app.use(errorHandler);
 
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    
+    res.send('API is running...')
+  })
+}
+
   const PORT = process.env.PORT || 5000;
   
   const server = app.listen(PORT, () => {
