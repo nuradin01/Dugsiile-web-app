a21d8e95e01b18e724b45ca13bc7db36eb794aee
Nuradin Ismail <nuradin170@gmail.com>
2020-12-03 17:34:29 +0300

    leave everything as it is

--- server.js
+++ server.js
@@ -67,7 +67,6 @@ app.use(errorHandler);
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
-  app.use(express.static('client/build/public/favicon.ico'));
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
